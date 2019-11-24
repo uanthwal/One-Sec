3e6a3c92b65c0b743378fe34c471055df7bb6a3e
Avinash Gazula <av530575@dal.ca>
2019-11-24 01:28:50 -0400

    added Location detection

--- app/src/main/java/com/mobilecomputing/one_sec/activities/TestActivity.java
+++ app/src/main/java/com/mobilecomputing/one_sec/activities/TestActivity.java
@@ -15,6 +15,7 @@ import java.security.SecureRandom;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.mobilecomputing.one_sec.R;
@@ -22,9 +23,16 @@ import com.warrenstrange.googleauth.GoogleAuthenticator;
 
 import org.jboss.aerogear.security.otp.Totp;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class TestActivity extends AppCompatActivity implements LocationListener {
     protected LocationManager locationManager;
     protected LocationListener locationListener;
+
+    private static final String[] LOCATION_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
 
     TextView latitude;
     TextView longitude;
@@ -37,9 +45,6 @@ public class TestActivity extends AppCompatActivity implements LocationListener 
         longitude = findViewById(R.id.longitude);
 
         getLocation();
-        String secretKey = "GAUD NDRV NY6E ZISK 7V66 BH6H 3YL7 I75D PQ3V QLVP EPRM BFY3 7YTQ";
-        Totp generator = new Totp(secretKey);
-        latitude.setText(generator.now());
 
     }
 
@@ -48,16 +53,24 @@ public class TestActivity extends AppCompatActivity implements LocationListener 
 
     private void getLocation() {
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if ( ContextCompat.checkSelfPermission(TestActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        if(ActivityCompat.checkSelfPermission(TestActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            System.out.println("No permissions");
+            requestPermissions(LOCATION_PERMS, 190);
         }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
 
     }
 
     @Override
     public void onLocationChanged(Location location) {
+        if(location.getLatitude()<50){
+            setContentView(R.layout.test_layoout);
+        }else{
+            setContentView(R.layout.test_layout1);
+        }
         latitude.setText(String.valueOf(location.getLatitude()));
-        longitude.setText(String.valueOf(location.getLatitude()));
+        longitude.setText(String.valueOf(location.getLongitude()));
         Toast.makeText(getApplicationContext(), String.valueOf(location.getLatitude()), Toast.LENGTH_LONG).show();
     }
 
@@ -76,15 +89,20 @@ public class TestActivity extends AppCompatActivity implements LocationListener 
 
     }
 
-    public final class CryptoProvider extends Provider {
-        /**
-         * Creates a Provider and puts parameters
-         */
-        public CryptoProvider() {
-            super("Crypto", 1.0, "HARMONY (SHA1 digest; SecureRandom; SHA1withDSA signature)");
-            put("SecureRandom.SHA1PRNG",
-                    "org.apache.harmony.security.provider.crypto.SHA1PRNG_SecureRandomImpl");
-            put("SecureRandom.SHA1PRNG ImplementedIn", "Software");
+    //Returns distance between two points in metres
+    private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515 * 1.609344 * 1000;
+            return (dist);
         }
     }
+
+
 }
